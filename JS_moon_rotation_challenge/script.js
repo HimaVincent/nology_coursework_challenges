e8f51c7e8abd0931b97d63e0fbb1084c2a68b6ed
HimaVincent <himavincent@outlook.com>
2025-11-18 23:47:18 +1100

    JavaScript Challenge: Calculating moon Rotations

---
+++ JS_moon_rotation_challenge/script.js
@@ -0,0 +1,4 @@
+let earthDays = 90;
+let moonRotationDays = 27.3;
+let numberOfRotations = earthDays / moonRotationDays;
+console.log(`In ${earthDays} Earth days, the Moon completes ${numberOfRotations.toFixed(2)} rotations.`)
