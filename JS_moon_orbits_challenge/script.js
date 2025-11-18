74f07aea8b8b1e61e1915e6d334f35d8ce7478b2
HimaVincent <himavincent@outlook.com>
2025-11-19 01:04:49 +1100

    JavaScript Challenge: Calculating Moon orbits

---
+++ JS_moon_orbits_challenge/script.js
@@ -0,0 +1,4 @@
+let earthDays = 120;
+let moonOrbitsEarthDays = 27.3;
+let numberOforbits = earthDays / moonOrbitsEarthDays ;
+console.log(`The Moon orbits the Earth ${numberOforbits.toFixed(2)} times in ${earthDays} earth days.`);
